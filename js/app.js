c0ff15d95fc9ed419f4ce2131f184dc574b0218b
lallpress <lallpress@unomaha.edu>
2017-10-08 22:44:10 -0500

    Separated out the scripts from FirstPage.html into app.js to <strong>temporarily</strong> accept a .txt file and display its contents. Will eventually only accept xml (or whatevs) and then do a thing to it.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    var fileInput = document.getElementById('fileInput');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+    fileInput.addEventListener('change', function(e) {
+        var file = fileInput.files[0];
+        var textType = /text.*/;
+
+        if(file.type.match(textType)) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                fileDisplayArea.innerText = reader.result;
+            }
+            reader.readAsText(file);
+        } else {
+            fileDisplayArea.innerText = "File not supported";
+        }
+    });
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
