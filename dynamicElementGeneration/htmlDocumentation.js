5b35d647b92235aadeb4302f3d9f8b01d5e75648
Ben Sprunger <benj.sprunger@gmail.com>
2017-10-26 09:43:15 -0500

    Documentation added for htmlDocumentation and addAllInputs

---
+++ dynamicElementGeneration/htmlDocumentation.js
@@ -0,0 +1,73 @@
+/** 
+ * @file htmlDoumentation.js
+ * @author Lauren Allpress
+ * @description This is the launching point for the Methodist Workflow application.
+ */
+
+/*
+<!doctype html>
+<html lang="en">
+<head>
+
+  <title>FirstPage</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <style>
+    .wholepage{
+        padding-top: 5px;
+        padding-bottom: 100px;
+        border: 3px solid black;
+
+    }
+    .containerheader{
+        text-align: center;
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+
+    .col1{
+        text-align: center;
+        padding-right: 20px;
+    }
+    .col2{
+        text-align: center;
+        padding-right: 250px;
+    }
+    .btn1{
+        width: 20em;
+        padding-right: 20px;
+    }
+
+  </style>
+</head>
+<body>
+    <div class="wholepage">
+        <div class="containerheader">
+            <h1>OPEN A WORKFLOW</h1><br>
+            <h5>"Workflow Name"</h5><br>
+            <div class="row1" style="display: flex; justify-content: space-around">
+                <div class="col1">
+                </div>
+                <div class="col2">
+                    <input type="file" id="fileInput">
+                </div>
+            </div>
+            <script src="/js/app.js"></script>
+            <div class="row2" style="text-align: center">
+                <h3>Choose A Step <br><br></h3>
+            </div>
+            <div class="row3" >
+                <select name="steps">
+                    <option value="step_1" selected>Step 1</option>
+                    <option value="step_2">Step 2</option>
+                    <option value="step_3">Step 3</option>
+                    <option value="Collect">Collect Data</option>
+                </select>
+
+                <input type="button" value="Load new document" onclick="newDoc()">
+                <script src="/js/app.js"></script>
+            </div>
+            </div>
+        </div>
+</body>
+</html>
+*/
