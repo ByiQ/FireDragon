b5411f11c753656be23ee9d17dced8f4e2d596f2
lallpress <lallpress@unomaha.edu>
2017-12-05 18:56:31 -0600

    Added simple search for patient records. Outputs name and mrn to html list.

--- src/app/app.js
+++ src/app/app.js
@@ -26,6 +26,22 @@ db.loadDatabase();
 //         }
 //     });
 // };
+function showResults() {
+    var list = document.getElementById("results_list");
+    var search_term = document.getElementById("search_box").value;
+
+    // Remove any items from a previous search.
+    list.innerHTML = "";
+    db.find({name: search_term}, function (err, docs) {
+        docs.forEach( function (arrayItem) {
+            var list_item = document.createElement('li');
+            list_item.setAttribute('name', arrayItem.name);
+            list_item.textContent = "MRN: " + arrayItem.mrn + "\t\tName: " + arrayItem.name;
+
+            list.appendChild(list_item);
+        });
+    });
+}
 
 function newDoc() {
     window.location.assign('./FormPage.html');
