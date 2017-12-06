b4d817af03cd8698677f31b36adacc9b472db919
lallpress <lallpress@unomaha.edu>
2017-12-05 19:36:16 -0600

    Added file type validation to file chooser. Removed deprecated code in JS file.

--- src/app/app.js
+++ src/app/app.js
@@ -5,27 +5,6 @@ var db = new Datastore({ filename: 'src/data/test.db' });
 
 db.loadDatabase();
 
-// File extension validation needs debugging
-// window.onload = function () {
-//     var fileInput = document.getElementById('fileInput');
-//     var fileDisplayArea = document.getElementById('fileDisplayArea');
-//
-//     fileInput.addEventListener('change', function(e) {
-//         var file = fileInput.files[0];
-//         var textType = /text.*/;
-//
-//         if(file.type.match(textType)) {
-//             var reader = new FileReader();
-//
-//             reader.onload = function (e) {
-//                 fileDisplayArea.innerText = reader.result;
-//             };
-//             reader.readAsText(file);
-//         } else {
-//             fileDisplayArea.innerText = "File not supported";
-//         }
-//     });
-// };
 function showResults() {
     var list = document.getElementById("results_list");
     var search_term = document.getElementById("search_box").value;
